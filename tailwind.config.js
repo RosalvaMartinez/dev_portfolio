0ac7487061d0dcd183927590b57cd2ad15149f71
Rosalva Martinez <rosalvahmartinez@gmail.com>
2023-06-22 05:36:20 +0100

    Add colors extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wavy-pink': 'f6eded',
+      }
+    },
   },
   plugins: [],
 }
